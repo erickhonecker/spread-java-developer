67b22268f874e86a78bd3546759cadfd3aecfec8
erick <erick_honecker@hotmail.com>
2021-12-20 01:23:45 -0300

    nó e encadeamento de nó

---
+++ Estrutura_de_Dados/src/main/java/br/com/dio/conceitos_basicos/nó_encadeamento_de_nó/No.java
@@ -0,0 +1,37 @@
+package br.com.dio.conceitos_basicos.nó_encadeamento_de_nó;
+
+public class No {
+
+    private String content;
+    private No proximoNo;
+
+    //construtor
+    public No(String content){
+        this.proximoNo = null;
+        this.content = content;
+    }
+
+    //Modificadores de Acesso -- Getters e Setters
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public No getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "content='" + content + '\'' +
+                '}';
+    }
+}
