f8fb2ba287151331a99805816629031f109da90d
erick <erick_honecker@hotmail.com>
2021-12-26 16:18:15 -0300

    metodos validaIndice e Add.

--- Estrutura-de-dados/src/main/java/listas_encadeadas/ListaEncadeada.java
+++ Estrutura-de-dados/src/main/java/listas_encadeadas/ListaEncadeada.java
@@ -8,6 +8,40 @@ public class ListaEncadeada<T> {
 		this.referenciaEntrada = null;
 	}
 	
+	
+	
+	// método Add 
+	public void add(T conteudo) {
+		No<T> novoNo = new No<>(conteudo);
+		
+		if(this.isEmpty()) {
+			referenciaEntrada = novoNo;
+			return;
+		}
+		
+		No<T> noAuxiliar = referenciaEntrada;
+		for(int i = 0; i < this.size()-1; i++) {
+			noAuxiliar = noAuxiliar.getProximoNo();
+		}
+		
+		noAuxiliar = noAuxiliar.getProximoNo();
+	}
+	
+	private No<T> getNo(int index){
+		
+		validaIndice(index);
+		No<T> noAuxiliar = referenciaEntrada;
+		No<T> noRetorno = null;
+		
+		for (int i = 0; i < this.size()-1; i++) {
+			noRetorno = noAuxiliar;
+			noAuxiliar = noAuxiliar.getProximoNo();
+		}
+		return noRetorno;
+	}
+	
+	
+	// método Size
 	public int size() {
 		int tamanhoLista = 0;
 		No<T> referenciaAux = referenciaEntrada;
@@ -28,6 +62,15 @@ public class ListaEncadeada<T> {
 		return tamanhoLista;
 	}
 	
+	public void validaIndice(int index ) {
+		if(index >= size()) {
+			int ultimoIndice = size() -1;
+			
+			throw new IndexOutOfBoundsException("Não Existe conteúdo no indice " + index + "desta lista. esta lista só vai até o indice"+ ultimoIndice + '.' );
+		}
+	}
+	
+	// método isEmpty
 	public boolean isEmpty() {
 		return referenciaEntrada == null? true : false;
 	}
