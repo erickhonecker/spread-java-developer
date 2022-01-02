e491d40fa49288516df42450a3e912c295e2ed0f
erickhonecker <erick_honecker@hotmail.com>
2022-01-02 15:41:47 -0300

    criação de objetos e chamadas de metodos no main

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.BootCamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -27,10 +29,44 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("BootCamp Java Developer");
+        bootCamp.setDescricao("Descrição BootCamp Java Developer");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        System.out.println("------------ Dev Camila ---------");
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootCamp);
+        System.out.println("Conteúdos Inscritos " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println(" ");
+        System.out.println("Conteúdos Inscritos " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devCamila.getConteudosConcluidos());
+        System.out.println("XP -->  " + devCamila.calcularTotalXp());
+        System.out.println("--------------------- \n");
+
+        Dev devJoao = new Dev();
+        System.out.println("------------ Dev João ---------");
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootCamp);
+        System.out.println("Conteúdos Inscritos " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println(" ");
+        System.out.println("Conteúdos Inscritos " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devJoao.getConteudosConcluidos());
+        System.out.println("XP -->  " + devJoao.calcularTotalXp());
+        System.out.println(" --------------");
 
         //impressões dos objetos
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+
+
     }
 }
